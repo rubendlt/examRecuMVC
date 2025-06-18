654df85876de3292dcf5a7f640defe4cfff880f9
Ruben dlt <rubendlt2006@gmail.com>
2025-06-18 13:28:35 +0200

    Controller iniciado

---
+++ src/Controller.java
@@ -0,0 +1,10 @@
+public class Controller {
+    private Model model;
+    private View view;
+
+    // Constructor que recibe el modelo y la vista
+    public Controller(Model model, View view) {
+        this.model = model;
+        this.view = view;
+    }
+}
